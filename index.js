a9bbb1621d336add8eddc749c0d2bb772dd858b2
softlolx <softlolx@gmail.com>
2022-05-19 20:13:29 +0300

    feature: time showing

---
+++ index.js
@@ -0,0 +1,18 @@
+let secondHand = document.querySelector(".clock__hand_type_second");
+let minuteHand = document.querySelector(".clock__hand_type_min");
+let hourHand = document.querySelector(".clock__hand_type_hour");
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+  const secondDegrees = (seconds / 60) * 360 + 90;
+  const minutesDegrees = (minutes / 60) * 360 + 90;
+  const hoursDegrees = (hours / 12) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
